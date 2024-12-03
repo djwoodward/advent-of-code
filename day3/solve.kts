7810e63a03d5f73be0b70587d27021c89c6fd5fc
Dan Woodward <dan.woodward@gravie.com>
2024-12-03 14:58:26 -0600

    Update solve.kts

--- day3/solve.kts
+++ day3/solve.kts
@@ -3,8 +3,24 @@ import java.nio.file.Path
 
 val text = Files.readString(Path.of("input.txt"))
 
-val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)")
+// The mul(a,b) instruction multiplies the value, assuming a,b are both 1-3 digit integers.
+// The do() instruction enables future mul instructions.
+// The don't() instruction disables future mul instructions.
 
-val result = mulRegex.findAll(text).fold(0) { acc, i -> acc + i.groupValues[1].toInt() * i.groupValues[2].toInt() }
-println("result = ${result}")
+val mulRegex = Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)")
+
+var accumulator = 0
+var isOn = true
+mulRegex.findAll(text).map({ it.value }).forEach { token ->
+    if (token == "do()") {
+        isOn = true
+    } else if (token == "don't()") {
+        isOn = false
+    } else if (isOn) {
+        val (a, b) = token.replace(Regex("[^\\d,]"), "").split(',').map { it.toInt() }
+        accumulator += a * b
+    }
+}
+
+println("result = ${accumulator}")
 
